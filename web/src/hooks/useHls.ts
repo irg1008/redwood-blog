b111543a8794789a7e85a462fbadac09593a9e9a
irg1008 <ivansudevlop@gmail.com>
2024-11-30 18:33:28 +0100

    chore(front/streams): update hls hook

--- web/src/hooks/useHls.ts
+++ web/src/hooks/useHls.ts
@@ -8,18 +8,16 @@ const AUTO_LEVEL = -1
 const AUTOPLAY = true
 const INITIAL_MUTED = true
 const INITIAL_VOLUME = INITIAL_MUTED ? 0 : 1
-const MIN_MUXED_HEIGHT = 480
 
 const hlsConfig: Partial<HlsConfig> = {
-  debug: false,
   enableWorker: true,
   lowLatencyMode: true,
   backBufferLength: 90,
-  maxBufferLength: 300,
   autoStartLoad: true,
   startLevel: 0, // Highest quality
-  liveSyncDuration: -2000, // Delay. Negative value to squeeze the buffer
-  liveMaxLatencyDuration: 10_000, // Max delay until seek live
+  liveSyncDuration: -1000, // Delay. Negative value to squeeze the buffer
+  maxBufferLength: 15,
+  maxMaxBufferLength: 60,
 }
 
 type HlsState = {
@@ -27,6 +25,7 @@ type HlsState = {
   currentQuality?: Level
   autoEnabled: boolean
   currentSpeed: number
+  timesLoading: number
   loading: boolean
   paused: boolean
   volume: number
@@ -47,11 +46,12 @@ type HlsState = {
   setPictureInPicture: (pictureInPicture: boolean) => void
 }
 
-const useHlsStore = create<HlsState>((set) => ({
+const useHlsStore = create<HlsState>((set, get) => ({
   qualities: [],
   autoEnabled: true,
   currentSpeed: 1,
-  loading: true,
+  loading: false,
+  timesLoading: 0,
   paused: !AUTOPLAY,
   volume: INITIAL_VOLUME,
   muted: INITIAL_MUTED,
@@ -62,7 +62,8 @@ const useHlsStore = create<HlsState>((set) => ({
   setAutoEnabled: (autoEnabled) => set({ autoEnabled }),
   setCurrentQuality: (currentQuality) => set({ currentQuality }),
   setCurrentSpeed: (currentSpeed) => set({ currentSpeed }),
-  setLoading: (loading) => set({ loading }),
+  setLoading: (loading) =>
+    set({ loading, timesLoading: get().timesLoading + 1 }),
   setPaused: (paused) => set({ paused }),
   setVolume: (volume) => set({ volume, muted: volume === 0 }),
   setMuted: (muted) => set({ muted }),
@@ -73,7 +74,7 @@ const useHlsStore = create<HlsState>((set) => ({
 
 export const useHls = (streamUrl: string) => {
   const videoRef = useRef<HTMLVideoElement>(null)
-  const hlsRef = useRef(new Hls(hlsConfig))
+  const hlsRef = useRef<Hls>(new Hls(hlsConfig))
 
   const {
     qualities,
@@ -87,6 +88,7 @@ export const useHls = (streamUrl: string) => {
     loading,
     fullscreen,
     pictureInPicture,
+    timesLoading,
     setQualities,
     setAutoEnabled,
     setLatency,
@@ -101,16 +103,18 @@ export const useHls = (streamUrl: string) => {
   } = useHlsStore()
 
   useEffect(() => {
-    if (!videoRef.current) return
-
     const video = videoRef.current
+    if (!video) return
+
     const hls = hlsRef.current
 
-    video.controls = false
     video.playsInline = true
     video.muted = INITIAL_MUTED
     video.autoplay = AUTOPLAY
     video.volume = INITIAL_VOLUME
+    video.crossOrigin = 'anonymous'
+    video.loop = false
+    video.preload = 'auto'
 
     if (!Hls.isSupported()) {
       videoRef.current.src = streamUrl
@@ -124,24 +128,12 @@ export const useHls = (streamUrl: string) => {
     })
 
     hls.on(Hls.Events.MANIFEST_PARSED, (_e, data) => {
-      const hasOneVideoTrack = data.levels.length === 1
-
-      if (hasOneVideoTrack) {
-        const canBeMuxed = data.levels[0].height > MIN_MUXED_HEIGHT
-
-        // If we only have one track and can be muxed (rescaled down on quality), wait for remuxed tracks to come by.
-        if (canBeMuxed) {
-          hls.recoverMediaError()
-          return
-        }
-      }
-
-      play()
-
       hls.levels.sort((a, b) => b.height - a.height)
       setQualities(data.levels)
       setAutoEnabled(hls.autoLevelEnabled)
       setLatency(hls.latency)
+
+      play()
     })
 
     hls.on(Hls.Events.LEVEL_SWITCHED, (_event, data) => {
@@ -151,10 +143,6 @@ export const useHls = (streamUrl: string) => {
 
     hls.on(Hls.Events.ERROR, (_event, err) => {
       console.error(err)
-    })
-
-    hls.on(Hls.Events.LEVELS_UPDATED, (_event, data) => {
-      console.log('levels updated', data)
     })
 
     video.addEventListener('waiting', () => {
@@ -222,15 +210,16 @@ export const useHls = (streamUrl: string) => {
   ])
 
   const seekLive = () => {
-    const livePosition = hlsRef.current.liveSyncPosition
+    const livePosition = hlsRef.current?.liveSyncPosition
     const video = videoRef.current
     if (!livePosition || !video) return
 
-    video.currentTime = livePosition
+    video.currentTime = Math.round(livePosition)
   }
 
   const setSelectedLevel = (level: number) => {
     const hls = hlsRef.current
+    if (!hls) return
 
     hls.currentLevel = level
     setAutoEnabled(hls.autoLevelEnabled)
@@ -311,16 +300,17 @@ export const useHls = (streamUrl: string) => {
   return {
     autoLevel: AUTO_LEVEL,
     autoEnabled,
-    selectedLevel: autoEnabled ? AUTO_LEVEL : hlsRef.current.currentLevel,
+    selectedLevel: autoEnabled ? AUTO_LEVEL : hlsRef.current!.currentLevel,
     videoRef,
     qualities,
     currentQuality,
     currentSpeed,
     loading,
+    timesLoading,
     paused,
     volume,
     latency,
-    playingDate: hlsRef.current.playingDate,
+    playingDate: hlsRef.current?.playingDate,
     muted,
     isHlsSupported: Hls.isSupported(),
     fullscreen,
